0bbe61f47dd32f2b4f59af3f2bfda778d5e7186e
wryk <milia@guerilla.studio>
2023-03-02 07:14:57 +0100

    add wip serverless support

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss")
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "serverless",
+		functionsDir: "./netlify/functions/",
+	});
+
 	eleventyConfig.addPlugin(eleventySass)
 
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough")
