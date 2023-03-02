18108837685fa81ee8a857e1ce785998cfa563ef
wryk <milia@guerilla.studio>
2023-03-02 01:47:27 +0100

    expose loading and error

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ import { createApp } from "https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es
 
 let internalCount = 0
 
-
 createApp({
 	App
 }).mount(document.body)
@@ -13,12 +12,30 @@ function App({ initialCount }) {
 
 	return {
 		count: initialCount,
+
+		get formattedCount() {
+			return this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+		},
+
 		participating: false,
+		error: false,
+		loading: false,
 
 		async submit(event) {
 			event.preventDefault()
-			this.count = await incrementCount()
-			this.participating = true
+
+			try {
+				this.loading = true
+				this.error = false
+
+				this.count = await incrementCount()
+				this.participating = true
+			} catch (error) {
+				this.error = true
+				console.log(error)
+			} finally {
+				this.loading = false
+			}
 		},
 
 		unsubscribeCount: null,
@@ -71,6 +88,11 @@ async function fetchCount() {
 
 async function incrementCount() {
 	await wait(1000)
+
+	if (!randomInt(0, 1)) {
+		throw new Error()
+	}
+
 	internalCount += 1 + randomInt(0, 9)
 	return internalCount
 }
