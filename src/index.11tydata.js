68d837cf603617311dfdf1afe44fca3a504e49c5
Tixie <tixie@guerilla.studio>
2023-05-24 16:28:57 +0200

    Post-mortem commit

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -1,7 +1,4 @@
-const fetch = require("node-fetch")
 
-module.exports = async function() {
-	const response = await fetch("https://c.compteurdegreve.fr/val")
-	const data = await response.json()
-	return {count: data.value}
+module.exports = function() {
+	return {count: 18014}
 }
